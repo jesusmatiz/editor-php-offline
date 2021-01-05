648e46499b25b388df49a311b06cec1bb8acdbcb
Jesús Matiz <jesusmatiz35@gmail.com>
2021-01-05 16:27:55 -0500

    Se agrega validación de extension y archivos para evitar eliminación

--- delete-file.php
+++ delete-file.php
@@ -1,3 +1,11 @@
 <?php
 
-unlink($_POST['filename']);
+$filename = $_POST['filename'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+if( $ext == "php" && 
+    $filename != "delete-file.php" && 
+    $filename != "file-write.php" && 
+    $filename != "index.php") {
+   unlink($_POST['filename']);
+}
